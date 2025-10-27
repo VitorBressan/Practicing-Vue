72f37f61aa05bc9887fea946133180f2b1097802
VitorBressan <contato.vbressan@gmail.com>
2025-10-27 15:28:09 -0300

    add a bit of small features using already registered knowledge

--- Estudo Vue/src/App.vue
+++ Estudo Vue/src/App.vue
@@ -3,7 +3,7 @@ import { ref, computed } from 'vue';
 
 //data
 const name = ref('Bressan');
-const href = ref('https://www.scrimba.com');
+const href = ref('https://orteil.dashnet.org/cookieclicker/');
 const disabled = ref(true);
 const attributes = {
   id: 'big_emoji',
@@ -17,29 +17,34 @@ const getFormattedDate = (date) => {
 const username = ref('VitorBressan');
 const event = ref('dblclick');
 const blockLeave = () => {
-  alert("You can't leave!");
+  alert("Follow me on github!");
 };
 
 const newItem = ref("");
 const characterCount = computed(()=>{
   return newItem.value.length
 });
+
 </script>
 
 <template>
   <span v-bind="attributes">👋🏻</span>
   <h1>
     Hi, my name is 
-    <a v-on:click.prevent="blockLeave" :href="'https://github.com/' + username" target="_blank">{{ name }}</a>!
+    <a v-on:click="blockLeave" :href="'https://github.com/' + username" target="_blank">{{ name }}</a>!
     What is yours ?
   </h1>
   <input v-model="newItem" placeholder="Your name here">
   <p class="counter">
     {{ characterCount }}/200
   </p>
-  <a :href>
-    <button :disabled>Click to visit my favourite website! If you can...</button>
+  <a :href target="_blank">
+    <button :disabled>Click to visit my favorite website! If you can...</button>
   </a>
+  <button v-on:click="disabled = !disabled">Toggle</button>
+  <p>{{ disabled ? "Disabled" : "Enabled" }}</p>
+  
+
   <h2 v-on:[event]="coding_years++">
     {{
       coding_years > 1 ? `I have been coding for ${coding_years} years.` : "Im a newbie"
