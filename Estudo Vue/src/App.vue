36a4115ea91dd9f22f68103afd44afc2fe203dbd
VitorBressan <contato.vbressan@gmail.com>
2025-10-28 08:45:59 -0300

    organize the code a bit and improve some variables name

--- Estudo Vue/src/App.vue
+++ Estudo Vue/src/App.vue
@@ -1,43 +1,47 @@
 <script setup>
 import { ref, computed } from 'vue';
 
-//data
-const name = ref('Bressan');
-const href = ref('https://orteil.dashnet.org/cookieclicker/');
-const disabled = ref(true);
 const attributes = {
   id: 'big_emoji',
   class: 'waving',
 };
+
+const name = ref('Bressan');
+const href = ref('https://orteil.dashnet.org/cookieclicker/');
 const coding_years = ref(3);
 const getFormattedDate = (date) => {
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   return date.toLocaleDateString('en-UK', options);
 };
-const username = ref('VitorBressan');
+
+const githubUsername = ref('VitorBressan');
 const event = ref('dblclick');
-const blockLeave = () => {
+const disabled = ref(true);
+const blockLeaveGithub = () => {
   alert("Follow me on github!");
 };
 
-const newItem = ref("");
+const userTypedName = ref("");
 const characterCount = computed(()=>{
-  return newItem.value.length
+  return userTypedName.value.length
 });
-
 </script>
 
 <template>
   <span v-bind="attributes">👋🏻</span>
+
+
   <h1>
     Hi, my name is 
-    <a v-on:click="blockLeave" :href="'https://github.com/' + username" target="_blank">{{ name }}</a>!
+    <a v-on:click="blockLeaveGithub" :href="'https://github.com/' + githubUsername" target="_blank">{{ name }}</a>!
     What is yours ?
   </h1>
-  <input v-model="newItem" placeholder="Your name here">
+  <input v-model="userTypedName" placeholder="Your name here">
   <p class="counter">
     {{ characterCount }}/200
   </p>
+
+
   <a :href target="_blank">
     <button :disabled>Click to visit my favorite website! If you can...</button>
   </a>
